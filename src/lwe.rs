ac582f34da8bdc5ca463ea4d9548db78bee22902
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2020-02-27 14:47:36 +0100

    Add decryption function and test, add rand_distr dependency to fix deprecated use of normal distribution
    Test is very flaky and works around one out of 12 (?) times

--- src/lwe.rs
+++ src/lwe.rs
@@ -1,14 +1,25 @@
 use crate::tlwe::{TLweParameters, Torus32};
 use crate::tsgw::{TGswKey, TGswParams, TGswSample, TGswSampleFFT};
 
-use crate::numerics::gaussian32;
+use crate::numerics::{approximate_phase, gaussian32};
 
+#[derive(Clone)]
 pub struct LweSample {
   /// The coefficients of the mask
   coefficients: Vec<Torus32>,
   b: Torus32,
   /// Average noise of the sample
   current_variance: f64,
+}
+
+impl LweSample {
+  pub fn new(params: &LweParams) -> Self {
+    Self {
+      coefficients: vec![0; params.n as usize],
+      b: 0,
+      current_variance: 0f64,
+    }
+  }
 }
 
 #[derive(Clone)]
@@ -88,7 +99,7 @@ impl LweKey {
   /// This function generates a random Lwe key for the given parameters.
   /// The Lwe key for the result must be allocated and initialized
   /// (this means that the parameters are already in the result)
-  pub fn generate(params: LweParams) -> Self {
+  pub fn generate(params: &LweParams) -> Self {
     use rand::distributions::Distribution;
 
     let n = params.n;
@@ -98,7 +109,10 @@ impl LweKey {
     let mut rng = rand::thread_rng();
     let key = (0..n).map(|_| d.sample(&mut rng)).collect();
 
-    Self { params, key }
+    Self {
+      params: params.clone(),
+      key,
+    }
   }
 
   /// This function encrypts message by using key, with stdev alpha
@@ -109,11 +123,15 @@ impl LweKey {
 
     let n = self.params.n;
     result.b = gaussian32(message, alpha);
-    let d = rand::distributions::Uniform::new(i32::min_value(), i32::max_value());
+    let d = rand_distr::Uniform::new(i32::min_value(), i32::max_value());
     let mut rng = rand::thread_rng();
     for i in 0..n {
       result.coefficients[i as usize] = d.sample(&mut rng);
-      result.b += result.coefficients[i as usize] * self.key[i as usize];
+
+      // Overflowed here, using wrapping add to imitate C++ behavior
+      result.b = result
+        .b
+        .wrapping_add(result.coefficients[i as usize] * self.key[i as usize]);
     }
     result.current_variance = alpha * alpha;
   }
@@ -134,7 +152,7 @@ impl LweKey {
 
     let n = self.params.n;
     result.b = message + f64_to_torus_32(noise);
-    let d = rand::distributions::Uniform::new(i32::min_value(), i32::max_value());
+    let d = rand_distr::Uniform::new(i32::min_value(), i32::max_value());
     let mut rng = rand::thread_rng();
     for i in 0..n {
       result.coefficients[i as usize] = d.sample(&mut rng);
@@ -142,6 +160,33 @@ impl LweKey {
     }
     result.current_variance = alpha * alpha;
   }
+
+  /**
+   * This function computes the decryption of sample by using key
+   * The constant Msize indicates the message space and is used to approximate the phase
+   */
+  pub fn decrypt(&self, sample: &LweSample, message_size: i32) -> Torus32 {
+    let phi = lwe_phase(sample, self);
+    approximate_phase(phi, message_size)
+  }
+}
+
+/**
+ * This function computes the phase of sample by using key : phi = b - a.s
+ */
+pub(crate) fn lwe_phase(sample: &LweSample, key: &LweKey) -> Torus32 {
+  let n = key.params.n;
+  let mut axs: Torus32 = 0;
+  let a: &Vec<Torus32> = &sample.coefficients;
+  let k = &key.key;
+
+  for i in 0..n {
+    // Overflowed here, using wrapping add to imitate C++ behavior
+    axs = axs.wrapping_add(a[i as usize] * k[i as usize]);
+  }
+
+  // Overflowed here, using wrapping sub to imitate C++ behavior
+  sample.b.wrapping_sub(axs)
 }
 
 //this pub structure contains Lwe parameters
@@ -229,10 +274,23 @@ mod tests {
     ]
   }
 
+  fn generate_keys() -> Vec<LweKey> {
+    let params = generate_parameters();
+    params.iter().map(LweKey::generate).collect()
+  }
+
+  /// `| frac(x) |`
+  ///
+  /// Frac:
+  /// Return the integer nearest X in the direction of the prevailing rounding mode. (Default in C++ is `FE_TONEAREST`)
+  fn abs_frac(x: f64) -> f64 {
+    x.round().abs()
+  }
+
   #[test]
   fn assert_lwe_key() {
     for param in generate_parameters() {
-      let key = LweKey::generate(param);
+      let key = LweKey::generate(&param);
       let n = key.params.n;
       let s = key.key;
       // Ensure key is binary
@@ -248,9 +306,49 @@ mod tests {
     }
   }
 
+  /// This function encrypts message by using key, with stdev alpha
+  /// The Lwe sample for the result must be allocated and initialized
+  /// (this means that the parameters are already in the result)
   #[test]
-  #[ignore]
   fn test_encryption_decryption() {
-    unimplemented!()
+    use crate::numerics::{mod_switch_to_torus32, torus_32_to_f64};
+    use std::collections::HashSet;
+
+    const NB_SAMPLES: i32 = 10;
+    const M: i32 = 8;
+    const ALPHA: f64 = 1f64 / (10f64 * (M as f64));
+
+    for key in generate_keys() {
+      let params = key.params.clone();
+      let mut samples = vec![LweSample::new(&params); NB_SAMPLES as usize];
+
+      // Verify correctness of the decryption
+      for trial in 0..NB_SAMPLES {
+        let message: Torus32 = mod_switch_to_torus32(trial, M);
+        key.encrypt(&mut samples[trial as usize], message, ALPHA);
+        let phase: Torus32 = lwe_phase(&samples[trial as usize], &key);
+        let decrypted = key.decrypt(&samples[trial as usize], M);
+        let decrypted_message = torus_32_to_f64(message);
+        let decrypted_phase = torus_32_to_f64(phase);
+        assert_eq!(message, decrypted);
+        println!(
+          "absfrac {} <= {}",
+          abs_frac(decrypted_message - decrypted_phase),
+          10f64 * ALPHA
+        );
+        assert!(abs_frac(decrypted_message - decrypted_phase) <= 10f64 * ALPHA);
+        assert_eq!(ALPHA * ALPHA, samples[trial as usize].current_variance);
+      }
+
+      // Verify that samples are random enough (all coordinates different)
+      let n = params.n;
+      for i in 0..n {
+        let mut test_set = HashSet::new();
+        for sample in samples.iter() {
+          test_set.insert(sample.coefficients[i as usize]);
+        }
+        assert!(test_set.len() as f64 >= 0.9 * NB_SAMPLES as f64);
+      }
+    }
   }
 }
--- src/numerics.rs
+++ src/numerics.rs
@@ -5,18 +5,63 @@ pub(crate) fn gaussian32(message: Torus32, sigma: f64) -> Torus32 {
   use rand::distributions::Distribution;
 
   // Attention: all the implementation will use the stdev instead of the gaussian fourier param
-  let d = rand::distributions::Normal::new(0f64, sigma);
+  let d = rand_distr::Normal::new(0f64, sigma).expect("Could not create normal distribution");
   let mut rng = rand::thread_rng();
   let error = d.sample(&mut rng);
-  message + f64_to_torus_32(error)
+
+  // Overflowed here, using wrapping add to imitate C++ behavior
+  message.wrapping_add(f64_to_torus_32(error))
 }
 
 /// # Warning
 /// Weird and lossy conversion
 pub(crate) fn f64_to_torus_32(d: f64) -> Torus32 {
-  const TWO_32: i64 = 4294967296; // 2 ^ 32
+  // 2 ^ 32
+  const TWO_32: i64 = 4294967296;
   let inner = d - (d as i64) as f64;
   let x = (inner * TWO_32 as f64) as i64;
   x as Torus32
   // C++ conversion: // return int32_t(int64_t((d - int64_t(d))*_two32));
+}
+
+pub(crate) fn torus_32_to_f64(x: Torus32) -> f64 {
+  // 2 ^ 32
+  const TWO_32: f64 = 4294967296.0;
+  (x as f64) / TWO_32
+}
+
+/// Used to approximate the phase to the nearest message possible in the message space
+/// The constant Msize will indicate on which message space we are working (how many messages possible)
+///
+/// "travailler sur 63 bits au lieu de 64, car dans nos cas pratiques, c'est plus précis"
+pub(crate) fn approximate_phase(phase: Torus32, message_size: i32) -> Torus32 {
+  // Width of each interval
+  let interval: u64 = ((1u64 << 63) / (message_size as u64)) * 2;
+
+  // Beginning of the first interval
+  let half_interval: u64 = interval / 2;
+
+  // Overflowed here, using wrapping add to imitate C++ behavior
+  let mut phase64: u64 = ((phase as u64) << 32).wrapping_add(half_interval);
+
+  // Floor to the nearest multiples of interval
+  phase64 -= phase64 % interval;
+
+  // Rescale to torus32
+  (phase64 >> 32) as Torus32
+}
+
+/// Used to approximate the phase to the nearest message possible in the message space
+/// The constant Msize will indicate on which message space we are working (how many messages possible)
+///
+/// "travailler sur 63 bits au lieu de 64, car dans nos cas pratiques, c'est plus précis"
+pub(crate) fn mod_switch_to_torus32(mu: i32, message_size: i32) -> Torus32 {
+  // Width of each interval
+  let interval: u64 = ((1u64 << 63) / message_size as u64) * 2;
+
+  // Overflowed here, using wrapping mul to imitate C++ behavior
+  let phase64: u64 = (mu as u64).wrapping_mul(interval);
+
+  // Floor to the nearest multiples of interval
+  (phase64 >> 32) as Torus32
 }
